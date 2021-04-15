8c3da8717155019e26911027f8fcbe495771cb33
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-04-15 19:44:20 -0400

    feat: Add logout button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton'
 
 function App() {
   return (
     <div className="App">
       <LoginButton></LoginButton>
+      <LogoutButton></LogoutButton>
     </div>
   );
 }
