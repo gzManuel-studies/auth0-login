9d487c73f4672d67702cbd3a2c54070bfe9106d1
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-04-15 19:59:36 -0400

    feat: Show Profile information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton'
+import Profile from './Profile';
 
 function App() {
   return (
     <div className="App">
       <LoginButton></LoginButton>
       <LogoutButton></LogoutButton>
+      <div>
+        <Profile></Profile>
+      </div>
     </div>
   );
 }
